4b8410836edb1d56a8fef35c7e16a711acde3494
Xiaoquan Kong <u1mail2me@gmail.com>
2018-09-18 11:05:57 +0800

    Add first workble codes

---
+++ index.js
@@ -0,0 +1,62 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        debug: false,
+        tokenizer_list: {},
+        message: "王小明在北京的清华大学读书。",
+        tokenizer_class: '',
+        token_list: {}
+    },
+    created: function () {},
+    method: {
+        send_tokenize_request: function () {
+            vm.axios.get('http://127.0.0.1:5000/single_tokenizer', {
+                'message': vm.message,
+                'tokenizer_class': vm.tokenizer_class
+            })
+                .then(function (response) {
+                    console.log(response.data);
+                    vm.token_list = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
+        }
+    }
+})
+
+// Optionally the request above could also be done as
+
+vm.axios.get('http://127.0.0.1:5000/list_tokenizer')
+    .then(function (response) {
+        console.log(response.data);
+        vm.tokenizer_list = response.data;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
+
+var send_tokenize_request = function () {
+    vm.axios.get('http://127.0.0.1:5000/single_tokenizer', {
+        params: {
+            'message': vm.message,
+            'tokenizer_class': vm.tokenizer_class
+        }
+    })
+        .then(function (response) {
+            console.log(response.data);
+            vm.token_list = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+}
